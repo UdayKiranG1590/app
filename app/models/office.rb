ebb98565ba8bd41c6de51a51baec69691d0a825c
UdayKiranG1590 <udaykirannlr1234@gmail.com>
2021-06-14 11:17:09 +0530

    callbackss

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,12 +1,12 @@
 class Office < ApplicationRecord
 	after_initialize do |office|
-		puts "you have initialized"
+		puts "you have initialized an object"
 	end
 	after_find do |office|
 		puts "you have found an object"
 	end
 	after_touch do |office|
-		puts "you have touched"
+		puts "you have touched an object"
 	end
 
 end
